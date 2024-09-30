6d54f26011cbc21e866c5f113ce2e0e61b81b12d
nemantro <slim.ayache@outlook.com>
2024-09-30 13:58:16 +0200

    Simple producer - consumer example with RabbitMQ

---
+++ Slim.Training.RabbitMQ.Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+using var host = builder.Build();
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+    routingKey: "hello",
+    basicProperties: null,
+    body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+await host.RunAsync();
